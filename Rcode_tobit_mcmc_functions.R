4a989fd7b2cd387d3fa77cdf9b05c2604e2841c3
kshirley <kennetheshirley@yahoo.com>
2015-02-05 07:54:00 -0500

    new files for holding R code

---
+++ Rcode_tobit_mcmc_functions.R
@@ -0,0 +1,283 @@
+# define student t density function:
+dst <- function(x, nu, mu, sigma) {
+  gamma((nu + 1)/2)/(gamma(nu/2)*sqrt(nu*pi*sigma^2))*(1 + 1/nu*((x - mu)/sigma)^2)^(-1*(nu + 1)/2)
+}
+
+# rwishart from package bayesm:
+rwishart <- function (nu,  V){
+  m = nrow(V)
+  df = (nu + nu - m + 1) - (nu - m + 1):nu
+  if (m  >  1) {
+	T = diag(sqrt(rchisq(c(rep(1, m)), df)))
+	T[lower.tri(T)] = rnorm((m * (m + 1)/2 - m))
+  } else {
+    T  =  sqrt(rchisq(1,  df))
+  }
+  U = chol(V)
+  C = t(T) %*% U
+  CI = backsolve(C, diag(m))
+  return(list(W = crossprod(C), IW = crossprod(t(CI)), C = C, CI = CI))
+}
+
+# sample from a truncated normal on one side using an exponential rejection sampling scheme:
+rtnorm.exp <- function(n=1,mean=0,sd=1,z.lower=-Inf,right=1,iter.max=10){
+  a.star <- (z.lower + sqrt(z.lower^2+4))/2
+  ret.val <- rep(NA,n)
+  w <- 1:n
+  for (i in 1:iter.max){
+    e <- rexp(n,rate=a.star) + z.lower
+    rho <- exp(-0.5*(e-a.star)^2)
+    u <- runif(n) < rho
+    ret.val[w][which(u[w])] <- e[w][which(u[w])]
+    n.new <- sum(!u[w])
+    if (n.new==0){    
+      break
+    } else {
+      #w <- which(!u[w])
+      w <- which(is.na(ret.val))
+    }
+  }
+  return(ret.val*(-1)^right*sd+mean)
+}
+
+# create exponential covariance matrix:
+R.cov <- function(lambda, d){
+  R <- diag(dim(d)[1])
+  R[upper.tri(R)] <- exp(-lambda*d[upper.tri(d)])
+  R[lower.tri(R)] <- exp(-lambda*d[lower.tri(d)])
+  return(R)
+}
+
+# new way to draw z:
+draw.W.start <- function(R, Z, beta.arc, Sigma, gamma.mat, X.arc, up, w.draw, W){
+  S <- length(R)
+  T <- dim(R[[1]])[2]
+  J <- unlist(lapply(R, dim))[seq(1, by = 2, length = S)]
+  for (s in 1:S){
+    mu.mat <- t(matrix(Z[, s], T, J[s]) + t(matrix(X.arc[[s]]*beta.arc[s], J[s], T)))  # J[s] x T matrix
+    for (j in 1:J[s]){ # draw j given -j
+      W[[s]][j, ] <- R[[s]][j, ]
+      cond.mu <- mu.mat[j, ]
+      cond.Sigma <- rep(Sigma[[s]][j, j], T)
+      z.score <- cond.mu/as.numeric(sqrt(cond.Sigma))
+      sel <- w.draw[[s]][j, ]
+      w2 <- which(z.score[sel] > 3.2 & up[[s]][j, sel] == 0) # high z.score and truncated on the right by zero:
+      w3 <- 1:sum(sel)
+      if (length(w2) > 0) w3 <- w3[-w2]
+      if (length(w3) > 0) W[[s]][j, sel][w3] <- rtnorm(n = length(w3), mean = cond.mu[sel][w3], sd = sqrt(cond.Sigma[sel][w3]), lower = -Inf, upper = up[[s]][j, sel][w3])
+      if (sum(is.na(W[[s]][j, ])) > 0) print(paste("Iteration", g, "NA in W w3"))
+      if (sum(W[[s]][j, ] == Inf) > 0) print(paste("Iteration", g, "Inf in W w3"))
+      if (sum(W[[s]][j, ] == -Inf) > 0) print(paste("Iteration", g, "-Inf in W w3"))
+      if (length(w2) > 0) W[[s]][j, sel][w2] <- rtnorm.exp(n = length(w2), mean = cond.mu[sel][w2], sd = sqrt(cond.Sigma[sel][w2]), z.lower = z.score[sel][w2], right = 1, iter.max = 100)
+      if (sum(is.na(W[[s]][j, ])) > 0) print(paste("Iteration", g, "NA in W w2"))
+      if (sum(W[[s]][j, ] == Inf) > 0) print(paste("Iteration", g, "Inf in W w2"))
+      if (sum(W[[s]][j, ] == -Inf) > 0) print(paste("Iteration", g, "-Inf in W w2"))
+    }
+  }
+  return(W)
+}
+
+# draw gamma:
+draw.gamma <- function(S, Z, X.arc, beta.arc, T, J, Sigma, W, alpha, gamma.temp){
+  for (s in 1:S){      
+    mu.W <- t(Z[, s] + matrix(rep(X.arc[[s]]*beta.arc[s], each = T), ncol = J[s]))
+    S.inv <- solve(Sigma[[s]])
+    lhs <- t(W[[s]] - mu.W)
+    out <- .C("draw_gamma", params = as.integer(c(J[s], T)), Sinv_in = as.double(S.inv), lhs_in = as.double(lhs), rate_in = as.double(rep(0, T)), alpha_in = as.double(alpha))
+    rate.vec <- out$rate_in
+    gamma.temp[, s] <- rgamma(T, shape = (J[s]+alpha)/2, rate = rate.vec)
+  }
+  return(gamma.temp)
+}
+
+# Draw Z | W,  gamma,  beta.arc,  Sigma,  beta,  lambda,  tau:
+draw.Z <- function(S, Sigma, T, J, X.arc, beta.arc, W, lambda, d.mat, tau, X, beta, gamma.mat){
+  # Compute mean vector for each draw,  from the data:
+  sigma.z <- numeric(S)
+  for (s in 1:S) sigma.z[s] <- sum(solve(Sigma[[s]]))
+  mu.z <- matrix(0, S, T)
+  for (s in 1:S) mu.z[s, ] <- (matrix(-1, 1, J[s]) %*% solve(Sigma[[s]]) %*% (matrix(X.arc[[s]]*beta.arc[s], J[s], T) - W[[s]]))/sigma.z[s]
+  # Compute prior covariance:
+  prior.inv <- solve(R.cov(lambda, d.mat))/tau^2
+  # Compute prior mean of multivariate z (from beta):
+  Pxb <- prior.inv %*% t(X%*%beta)
+  # Compute covariance matrix for each time point (an (S x S x T) array?)
+  Sig <- t(gamma.mat)*sigma.z # Here,  Sig is the inverse of Sigma^z_t,  i.e. the data precision matrix
+  # Multiply Sig by mu.z to get the left addition element in the right hand side of the mean of z_t:
+  Sig.mu <- Sig*mu.z
+  z.draw <- .C("zdraw", params = as.integer(c(S, T)), Smu_in = as.double(Sig.mu), Pxb_in = as.double(Pxb), Sig_in = as.double(Sig), Prior_in = as.double(prior.inv))
+  Z.out <- t(matrix(z.draw$Smu_in, S, T))
+  return(Z.out)
+}
+
+
+# Draw beta.arc | W,  gamma,  Z,  Sigma,  mu.arc,  tau.arc:
+draw.beta.arc <- function(S, Sigma, gamma.mat, X.arc, Z, T, J, W, tau.arc, mu.arc){
+  beta.arc.out <- numeric(S)
+  mu.ba <- numeric(S); sigma.ba <- numeric(S)
+  for (s in 1:S){
+    solve.S <- solve(Sigma[[s]])
+    denom <- sum(gamma.mat[, s])*t(X.arc[[s]]) %*% solve.S %*% X.arc[[s]]
+    numer <- -t(X.arc[[s]]) %*% solve.S %*% apply(gamma.mat[, s]*(matrix(Z[, s], T, J[s])-t(W[[s]])), 2, sum)
+    mu.w <- numer/denom
+    ssq.w <- 1/denom
+    d2 <- 1/ssq.w + 1/tau.arc^2
+    mu.ba[s] <- (mu.w/ssq.w + mu.arc/tau.arc^2)/d2
+    sigma.ba[s] <- sqrt(1/d2)
+    beta.arc.out[s] <- rnorm(1, mu.ba[s], sigma.ba[s])
+  }
+  return(beta.arc.out)
+}  
+
+
+# draw tau.alpha with a metropolis step:
+ta.draw <- function(alpha,mu.alpha,tau.alpha,jump.ta,g,adapt){
+  ll.data <- sum(log(dnorm(alpha,mu.alpha,tau.alpha)))
+  lp.old <- ll.data + log(dst(tau.alpha,nu=3,mu=0,sigma=0.5))
+  tau.alpha.star <- abs(tau.alpha + rnorm(1,0,jump.ta))
+  ll.data.star <- sum(log(dnorm(alpha,mu.alpha,tau.alpha.star)))
+  lp.star <- ll.data.star + log(dst(tau.alpha.star,nu=3,mu=0,sigma=0.5))
+  r <- exp(lp.star - lp.old)
+  if (g < adapt) jump.ta <- ifelse(r > 0.44,jump.ta*A1,jump.ta*B1)
+  #if (runif(1) < 0.44) tau.alpha <- tau.alpha.star
+  if (runif(1) < r) tau.alpha <- tau.alpha.star
+  return(list(tau.alpha=tau.alpha,jump.ta=jump.ta))
+}
+
+# Sample Sigma | W,  mu (Z,  X.arc,  beta.arc),  gamma:
+draw.Sigma <- function(S, Z, X.arc, beta.arc, T, J, W, gamma.mat, v.0, Lambda.0.inv, Sigma.null){
+  for (s in 1:S){
+    mu.W <- t(Z[, s] + matrix(rep(X.arc[[s]]*beta.arc[s], each = T), ncol = J[s]))      
+    SS <- (t(t(W[[s]])*gamma.mat[, s]) - t(t(mu.W)*gamma.mat[, s])) %*% t(W[[s]]-mu.W)
+  	Sigma.null[[s]] <- rwishart(nu  =  v.0[s] + T,  V  =  solve(Lambda.0.inv[[s]] + SS))$IW
+  }
+  return(Sigma.null)
+}
+
+# draw beta given z,  R:
+draw.beta.tobit <- function(tau, lambda, vec, xtx.ones, Z, X, sigma, mu){
+  Covar <- tau^2*R.cov(lambda, d.mat)
+  S <- dim(Covar)[1]
+  P <- dim(beta)[1]
+  Covar.inv <- solve(Covar)
+  B <- matrix(Covar.inv[vec]*as.numeric(xtx.ones), S*P, S*P)
+  W.temp <- matrix(0, P, S)   
+  for (j in 1:P){
+    for (s in 1:S){
+      temp <- 0
+      for (l in 1:S) temp <- temp + Covar.inv[s, l]*sum(Z[, l]*X[, j])
+      W.temp[j, s] <- W.temp[j, s] + temp
+    }
+  }
+  W.temp <- matrix(W.temp, ncol = 1)
+  sig.prior <- diag(1/rep(sigma^2, S))
+  B <- B + sig.prior
+  B.inv <- solve(B)
+  beta.prior <- rep(mu, S)
+  beta.hat <- B.inv %*% (W.temp + sig.prior %*% beta.prior)    
+  beta <- matrix(mvrnorm(1, beta.hat, B.inv), P, S)
+  return(list(beta = beta, B = B))
+}
+
+tau.draw.tobit <- function(Z, beta, lambda, a, b){
+  b.post <- b + 0.5*sum(((Z-X%*%beta) %*% solve(R.cov(lambda, d.mat)))*(Z-X%*%beta))
+  a.post <- a + T*S*0.5
+  return(sqrt(1/rgamma(1, shape = a.post, rate = b.post)))
+}
+
+
+# draw lambda with a metropolis step:
+lambda.draw.tobit <- function(X, beta, tau, lambda, d.mat, Z, S, jump.lambda){
+  mu.Z <- X %*% beta
+  Covar <- tau^2*R.cov(lambda, d.mat)
+  ll.data <- sum(dmvnorm(Z-mu.Z, rep(0, S), Covar, log = TRUE))
+  lp.old <- ll.data + log(dgamma(lambda, shape = 50, scale = 0.03))
+  lambda.star <- abs(lambda + rnorm(1, 0, jump.lambda))
+  Covar.star <- tau^2*R.cov(lambda.star, d.mat)
+  ll.data.star <- sum(dmvnorm(Z-mu.Z, rep(0, S), Covar.star, log = TRUE)) 
+  lp.star <- ll.data.star + log(dgamma(lambda.star, shape = 50, scale = 0.03))
+  r <- exp(lp.star - lp.old)
+  if (g  <  adapt) jump.lambda <- ifelse(r  >  0.44, jump.lambda*A1, jump.lambda*B1)
+  if (runif(1)  <  r) lambda <- lambda.star
+  return(list(lambda = lambda, jump.lambda = jump.lambda))
+}
+
+
+# script for metropolis-hastings draw of mean and sds of beta:
+mu.sigma.draw <- function(beta,mu,sigma,S,P,jump.sigma,g,adapt){
+  ll.data <- apply(matrix(dnorm(beta,mu,sigma,log=TRUE),ncol=S),1,sum)
+  lp.old <- ll.data + log(dst(sigma,nu=3,mu=0,sigma=1))
+  sigma.star <- abs(sigma+rnorm(P,0,jump.sigma))
+  ll.data.star <- apply(matrix(dnorm(beta,mu,sigma.star,log=TRUE),ncol=S),1,sum)    
+  lp.star <- ll.data.star + log(dst(sigma.star,nu=3,mu=0,sigma=1))
+  r <- exp(lp.star - lp.old)
+  if (g < adapt) jump.sigma <- ifelse(r > 0.44,jump.sigma*A1,jump.sigma*B1)
+  promote <- runif(P) < r
+  sigma[promote] <- sigma.star[promote]
+  mu <- rnorm(P,apply(beta,1,mean),sigma/sqrt(S))
+  return(list(mu=mu,sigma=sigma,jump.sigma=jump.sigma))
+}
+
+# Draw W, the multivariate student-t random variable:
+draw.W <- function(R, Z, beta.arc, Sigma, gamma.mat, X.arc, W, up, w.draw){
+  S <- length(R)
+  T <- dim(R[[1]])[2]
+  J <- unlist(lapply(R, dim))[seq(1, by = 2, length = S)]
+  for (s in 1:S){
+    mu.mat <- t(matrix(Z[, s], T, J[s]) + matrix(rep(X.arc[[s]]*beta.arc[s], each = T), ncol = J[s]))  # J[s] x T matrix
+    for (j in 1:J[s]){ # draw j given -j
+      Sigma.temp <- Sigma[[s]][j, -j] %*% solve(Sigma[[s]][-j, -j])
+      cond.mu <- mu.mat[j, ] + Sigma.temp %*% (W[[s]][-j, ] - mu.mat[-j, ])
+      cond.Sigma <- (Sigma[[s]][j, j] - Sigma.temp %*% Sigma[[s]][-j, j])/gamma.mat[, s]
+      z.score <- cond.mu/as.numeric(sqrt(cond.Sigma))
+      sel <- w.draw[[s]][j, ]
+      w2 <- which(z.score[sel] > 3.2 & up[[s]][j, sel] == 0) # high z.score and truncated on the right by zero:
+      w3 <- 1:sum(sel)
+      if (length(w2) > 0) w3 <- w3[-w2]
+      if (length(w3) > 0) W[[s]][j, sel][w3] <- rtnorm(n = length(w3), mean = cond.mu[sel][w3], sd = sqrt(cond.Sigma[sel][w3]), lower = -Inf, upper = up[[s]][j, sel][w3])
+      if (sum(is.na(W[[s]][j, ])) > 0) print(paste("Iteration", g, "NA in W w3"))
+      if (sum(W[[s]][j, ] == Inf) > 0) print(paste("Iteration", g, "Inf in W w3"))
+      if (sum(W[[s]][j, ] == -Inf) > 0) print(paste("Iteration", g, "-Inf in W w3"))
+      if (length(w2) > 0) W[[s]][j, sel][w2] <- rtnorm.exp(n = length(w2), mean = cond.mu[sel][w2], sd = sqrt(cond.Sigma[sel][w2]), z.lower = z.score[sel][w2], right = 1, iter.max = 100)
+      if (sum(is.na(W[[s]][j, ])) > 0) print(paste("Iteration", g, "NA in W w2"))
+      if (sum(W[[s]][j, ] == Inf) > 0) print(paste("Iteration", g, "Inf in W w2"))
+      if (sum(W[[s]][j, ] == -Inf) > 0) print(paste("Iteration", g, "-Inf in W w2"))
+    }
+  }
+  return(W)
+}
+
+
+
+##### Not currently used #####
+
+# draw alpha with a metropolis step:
+alpha.draw <- function(gamma.mat, a, b, jump.alpha){
+  ll.data <- sum(dgamma(gamma.mat, shape = alpha/2, scale = 2/alpha, log = TRUE))
+  lp.old <- ll.data + dgamma(alpha, shape = a, scale = b, log = TRUE)
+  alpha.star <- abs(alpha + rnorm(1, 0, jump.alpha))
+  ll.data.star <- sum(dgamma(gamma.mat, shape = alpha.star/2, scale = 2/alpha.star, log = TRUE))
+  lp.star <- ll.data.star + dgamma(alpha.star, shape = a, scale = b, log = TRUE)
+  r <- exp(lp.star - lp.old)
+  if (g  <  adapt) jump.alpha <- ifelse(r  >  0.44, jump.alpha*A1, jump.alpha*B1)
+  if (runif(1)  <  r) alpha <- alpha.star
+  return(list(alpha = alpha, jump.alpha = jump.alpha))
+}
+
+# draw alpha with a metropolis step:
+alpha.draw.unif <- function(gamma.mat, a, b, jump.alpha){
+  ll.data <- sum(dgamma(gamma.mat, shape = alpha/2, scale = 2/alpha, log = TRUE))
+  lp.old <- ll.data + dunif(alpha, a, b, log = TRUE)
+  alpha.star <- alpha + rnorm(1, 0, jump.alpha)
+  if (alpha.star  >  a & alpha.star  <  b){
+    ll.data.star <- sum(dgamma(gamma.mat, shape = alpha.star/2, scale = 2/alpha.star, log = TRUE))
+    lp.star <- ll.data.star + dunif(alpha, a, b, log = TRUE)
+    r <- exp(lp.star - lp.old)
+  } else {
+    r <- 0
+  }
+  if (g  <  adapt) jump.alpha <- ifelse(r  >  0.44, jump.alpha*A1, jump.alpha*B1)
+  if (runif(1)  <  r) alpha <- alpha.star
+  return(list(alpha = alpha, jump.alpha = jump.alpha))
+}
+
---
+++ new/Rcode1_data_setup.R
@@ -0,0 +1,229 @@
+# clear workspace:
+rm(list=ls())
+
+# set working directory:
+setwd("~/Git/Rainfall/")
+
+
+
+
+### 1. The observed rainfall data ### 
+
+# Load in 15 time series for Ethiopia:
+load("ethiopia_full_data.RData")
+
+# this is a 15 x 18094 data.frame, where the rows correspond
+# to rainfall time series (they are labeled) and the columns
+# are days starting with 1/1/1961, ending 7/28/2010, 
+# excluding Feb 29 from all leap years: (1964, 1968, 1972, 1976, 
+# 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008)
+# This means 12 days are excluded.
+
+# check length of series:
+length(seq(as.Date("1961-01-01"), as.Date("2010-07-28"), by = 1))
+# 18106 days, including leap year days
+
+# our data:
+dim(data)[2]
+# 18094, 12 fewer days. Good, this matches the number of leap years
+
+# Re order the rows, to group by location:
+data <- data[c(6, 1, 7, 2, 8, 3, 9, 4, 10, 5, 11, 12, 13, 14, 15), ]
+
+# set the data to 1992 onward, so get rid of the first 31 years:
+data <- data[, (365*31 + 1):dim(data)[2]] # 1992 onward, all 15 sites
+
+# get location names:
+site.names <- unlist(strsplit(rownames(data), "_")[seq(1, by=2, length=6)])[seq(1, by=2, length=6)]
+
+# Define number of days in the time series:
+N <- dim(data)[2]
+
+# number of locations:
+S <- 6
+
+# number of series for each location
+J <- c(2, 2, 2, 2, 2, 5)
+
+# Create the datestring beginning at Jan 1 1992:
+# Manually add 5 leap days:
+date.string <- as.Date(1:(N + 5) - 1, origin = "1992-01-01")
+
+# Now define the indices of those 5 leap days to delete:
+leap.year.index <- 60 + c(0, 365*4 + 1, 365*8 + 2, 365*12 + 3, 365*16 + 4)
+date.string <- date.string[-leap.year.index]
+
+# Get month names and year names, and create vectors to store their value for each day:
+month.names <- unique(months(date.string))
+year.names <- unique(format(date.string, "%Y"))
+month.vec <- match(months(date.string), month.names)
+year.vec <- match(year.names, year.names)
+
+# Put the observed rainfall in a list with one element per location
+# where each list element is a matrix:
+Y <- as.list(rep(NA, S))
+for (s in 1:S) Y[[s]] <- data[1:J[s] + c(0, cumsum(J))[s], ]
+
+
+
+
+### 2. The lat-long location data ### 
+ll <- read.table("lat_long_details.csv", sep=",", header=TRUE)
+ll <- ll[c(3, 5, 4, 1, 2, 6), ]  # reorder to match site.names ordering
+d.mat <- matrix(NA, S, S)
+for (i in 1:S){
+  for (j in 1:S){
+    d.mat[i, j] <- sqrt((ll[i, "Lat"] - ll[j, "Lat"])^2 + (ll[i, "Long"] - ll[j, "Long"])^2)*108
+    # Q for self: where does the 108 come from there? Something about lat-long and distance in KM in ethiopia?
+  }
+}
+rownames(d.mat) <- ll[, 1]
+colnames(d.mat) <- ll[, 1]
+d.mat <- d.mat/100
+
+
+
+
+
+### 3. El Nino data ###
+
+# Read in Nino 3.4 index:
+# KV - this index may come from here: http://www.cgd.ucar.edu/cas/catalog/climind/TNI_N34/
+# data from 1871 to present, V1 is the year
+nino <- read.table("nino34.long.data", colClasses=rep("numeric", 13))
+
+# vectorize data into one column
+nino <- matrix(t(as.matrix(nino[, -1])), ncol=1)
+
+# Make all dates, 12 months from 1871 to 2010
+nino.dates <- paste(rep(1871:2010, each=12), unique(months(date.string)), sep=" ")
+
+# cut dates and data off at April 2010
+# nino only goes through April 2010 -- the last 8 values were "-99.99"
+nino <- nino[1:which(nino.dates == "2010 April")]
+nino.dates <- nino.dates[1:which(nino.dates == "2010 April")]
+
+# subselect just the months that align with the rainfall data
+# Start 3 months before first rainfall month
+# We'll use nino with a 3-month lag
+# Check in this, but in EDA I think the highest correlation with rainfall was
+# to use nino from 3 months prior.
+w <- which(nino.dates == "1991 October")
+nino <- nino[w:length(nino.dates)]
+nino.dates <- nino.dates[w:length(nino.dates)]
+
+# center nino at its mean across these 223 months:
+nino <- nino - mean(nino)
+month.mat <- matrix(0, N, 12)
+# repeat month data 18 times, add in 6 more months
+# this: c(rep(month.days, 18), month.days[1:6], 28), constructs 18 repeats of the months, 
+# plus the first 6 months plus one more element of 28
+# so nino[1:223] are the values for each of 223 months, which is the latter expression, 
+# so each value of nino will be repeated the number of days in the 223 months
+
+# number of days per month for a non-leap year:
+month.days <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+# Align Nino start date with 3 month lag to data's start date:
+month.mat[cbind(1:N, month.vec)] <- rep(nino, c(rep(month.days, 18), month.days[1:6], 28))
+# month.mat contains the 3-month-lagged, normalized nino temperatures for each day in the data
+
+
+
+
+
+### 4. ARC information ###
+
+# Set up ARC indicator variable for each series:
+arc <- c(1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0)
+
+# ARC indicator (X^\text{ARC} in the paper)
+# KV-ARC data is http://journals.ametsoc.org/doi/abs/10.1175/JCLI-D-12-00206.1
+# KV-"(ARC) project aims to create an independent climate data record of sea surface 
+# temperatures (SSTs) covering recent decades that can be used for climate change analysis"
+X.arc <- as.list(rep(NA, S))
+for (s in 1:S){
+  X.arc[[s]] <- matrix(0, J[s], 1)
+  X.arc[[s]][1, 1] <- 1
+}
+
+
+
+
+
+
+### 5. Design matrix for regression ###
+
+# Create periodic predictor variables:
+# KV - for each column (10 all together) he is creating a sine 
+# and a cosine
+# In one year, the wave goes up and down once, plot(sin.mat[,1])
+M <- 10
+m.seq <- c(1:10)
+sin.mat <- matrix(NA, N, M)
+cos.mat <- matrix(NA, N, M)
+for (m in 1:M){
+  sin.mat[, m] <- sin(2*pi*(1:N)*m.seq[m]/365)
+  cos.mat[, m] <- cos(2*pi*(1:N)*m.seq[m]/365)
+}
+
+# Create X_t, which is the same across sites:
+# KV - might be building a regression matrix
+X <- cbind(rep(1, N), (1:N)/N - 0.5, ((1:N)/N - 0.5)^2, sin.mat[, 1:4], cos.mat[, 1:4], month.mat)
+
+# KV-P is the number of trends in the time trend variable
+P <- dim(X)[2]
+X.names <- c("Intercept", "Time (linear)", "Time (quadratic)", "sin-1", "sin-2", "sin-3", "sin-4",
+             "cos-1", "cos-2", "cos-3", "cos-4", paste(month.names, "Nino", sep="-"))
+
+
+
+
+
+
+### 6. index the NA elements of the data set ###
+
+# KV - he is creating a matrix of ones and zeros somehow related to 
+# the NAs in the original data set, data
+# KV - so na.mat is a list of 6 matrices ... the first 5 matrices will be 
+# (6779 x 2) and the 6th will be (6779 x 5) based on groupings of the site data
+
+# membership of each series within locations:
+site.mat <- cbind(c(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 6, 6, 6),
+                  c(1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 3, 4, 5))
+
+na.mat <- as.list(rep(NA, S))  # 1 = missing
+for (s in 1:S){
+  na.mat[[s]] <- matrix(0, J[s], N)
+  na.mat[[s]][is.na(data[site.mat[, 1] == s, ])] <- 1
+}
+
+
+
+
+# Save the necessary data objects in a named list:
+input.data <- list(Y = Y,  # observed rainfall
+                   site.names = site.names,  # names of sites
+                   N = N,  # number of time points
+                   X.arc = X.arc,  # list of ARC indicators
+                   S = S,  # number of locations
+                   J = J,  # number of time series per location
+                   date.string = date.string,  # vector of dates
+                   month.vec = month.vec,  # vector of month indices
+                   year.vec = year.vec,  # vector of year indices
+                   d.mat = d.mat,  # distance matrix
+                   X = X,  # design matrix for spatial regression
+                   P = P,  # number of predictors in X
+                   na.mat = na.mat  # list of NA indicators for observed rainfall
+                   )
+
+save(input.data, file="input_data.RData")
+
+
+
+
+
+
+
+
+
